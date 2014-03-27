0869844b32edf697f44f1d253568875591c905d4
jeff ward <jeff.ward@mediahive.com>
2014-03-26 23:38:31 -0400

    First Commit

---
+++ svfmemorialcup-root/config/secrets.js
@@ -0,0 +1,106 @@
+module.exports = {
+  db: process.env.MONGODB|| 'mongodb://localhost:27017/test',
+
+  sessionSecret: process.env.SESSION_SECRET || 'poop',
+
+  localAuth: true,
+
+  mailgun: {
+    login: process.env.MAILGUN_LOGIN || 'postmaster@sandbox42747.mailgun.org',
+    password: process.env.MAILGUN_PASSWORD || '8cd724yxm-u4'
+  },
+
+  sendgrid: {
+    user: process.env.SENDGRID_USER || 'Your SendGrid Username',
+    password: process.env.SENDGRID_PASSWORD || 'Your SendGrid Password'
+  },
+
+  nyt: {
+    key: process.env.NYT_KEY || 'Your New York Times API Key'
+  },
+
+  lastfm: {
+    api_key: process.env.LASTFM_KEY || 'Your API Key',
+    secret: process.env.LASTFM_SECRET || 'Your API Secret'
+  },
+
+  facebookAuth: true,
+  facebook: {
+    clientID: process.env.FACEBOOK_ID || '765342670150682',
+    clientSecret: process.env.FACEBOOK_SECRET || '094d8c8e6312a56ccf5328296f7d26ad',
+    callbackURL: '/auth/facebook/callback',
+    passReqToCallback: true
+  },
+
+  githubAuth: false,
+  github: {
+    clientID: process.env.GITHUB_ID || 'Your Client ID',
+    clientSecret: process.env.GITHUB_SECRET || 'Your Client Secret',
+    callbackURL: '/auth/github/callback',
+    passReqToCallback: true
+  },
+
+  twitterAuth: false,
+  twitter: {
+    consumerKey: process.env.TWITTER_KEY || 'Your Consumer Key',
+    consumerSecret: process.env.TWITTER_SECRET  || 'Your Consumer Secret',
+    callbackURL: '/auth/twitter/callback',
+    passReqToCallback: true
+  },
+
+  googleAuth: false,
+  google: {
+    clientID: process.env.GOOGLE_ID || 'Your Client ID',
+    clientSecret: process.env.GOOGLE_SECRET || 'Your Client Secret',
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
+  },
+
+  linkedinAuth: false,
+  linkedin: {
+    clientID: process.env.LINKEDIN_ID || 'Your Client ID',
+    clientSecret: process.env.LINKEDIN_SECRET || 'Your Client Secret',
+    callbackURL: '/auth/linkedin/callback',
+    scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
+    passReqToCallback: true
+  },
+
+  steam: {
+    apiKey: process.env.STEAM_KEY || 'Your Steam API Key'
+  },
+
+  twilio: {
+    sid: process.env.TWILIO_SID || 'AC8b7c61a14ec4f1e0821faaa0c3fc70ee',
+    token: process.env.TWILIO_TOKEN || '346693c57daed14aa85ecbd3f20a9598'
+  },
+
+  clockwork: {
+    apiKey: process.env.CLOCKWORK_KEY || 'Your Clockwork SMS API Key'
+  },
+
+  tumblr: {
+    consumerKey: process.env.TUMBLR_KEY || 'Your Consumer Key',
+    consumerSecret: process.env.TUMBLR_SECRET || 'Your Consumer Secret',
+    callbackURL: '/auth/tumblr/callback'
+  },
+
+  foursquare: {
+    clientId: process.env.FOURSQUARE_ID || 'Your Client ID',
+    clientSecret: process.env.FOURSQUARE_SECRET || 'Your Client Secret',
+    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || 'http://localhost:3000/auth/foursquare/callback'
+  },
+
+  venmo: {
+    clientId: process.env.VENMO_ID || 'Your Venmo Client ID',
+    clientSecret: process.env.VENMO_SECRET || 'Your Venmo Client Secret',
+    redirectUrl: process.env.VENMO_REDIRECT_URL || 'http://localhost:3000/auth/venmo/callback'
+  },
+
+  paypal: {
+    host: process.env.PAYPAL_HOST || 'api.sandbox.paypal.com',
+    client_id: process.env.PAYPAL_ID || 'Ab8yYxCnQyDlNhTKAhwOuDN68G26sjyc7D6HIk1DYM-Q_3QME3wEejs2ndUG',
+    client_secret: process.env.PAYPAL_SECRET || 'EFwg3BAM2QogdCF0kvOQPaY_mY4sEhz9LNUOwGbyBl31twWJ8oEL67Td19GQ',
+    returnUrl: process.env.PAYPAL_RETURN_URL || 'http://localhost:3000/api/paypal/success',
+    cancelUrl: process.env.PAYPAL_CANCEL_URL || 'http://localhost:3000/api/paypal/cancel'
+  }
+};
