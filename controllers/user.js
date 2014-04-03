c7dfe2d436d0fca5efd9fcbdfb3317e1f3dbd42b
jeff ward <jsward.17@gmail.com>
2014-04-03 15:59:30 -0400

    news page grid / signup form changes

--- controllers/user.js
+++ controllers/user.js
@@ -82,6 +82,7 @@ exports.getSignup = function(req, res) {
 exports.postSignup = function(req, res, next) {
   req.assert('email', 'Email is not valid').isEmail();
   req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('tel', 'Telephone must be at least 10 characters long').len(10);
   req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
 
   var errors = req.validationErrors();
@@ -93,7 +94,8 @@ exports.postSignup = function(req, res, next) {
 
   var user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    tel: req.body.tel
   });
 
   user.save(function(err) {
